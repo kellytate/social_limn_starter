70b013d98409040cb2e65d9c482c04eeb163c906
kelly <kelly.tate.dev@gmail.com>
2023-01-27 17:12:34 -0800

    Added basic profile page.

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('profile_list', views.profile_list, name='profile_list'),
+    path('profile/<int:pk>', views.profile, name='profile'),
+
+]
