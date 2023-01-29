1897bef93091717dbc044ecd318de58935d6f8cf
kelly <kelly.tate.dev@gmail.com>
2023-01-29 12:22:21 -0800

    Updated  imports

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import *
 from .models import Profile
