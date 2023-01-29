1a1f29598c56a866ccabb9033139641d68e402f5
maple-megan333 <maple.megan333@gmail.com>
2023-01-29 14:25:38 -0800

    Profile seralizer done.

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
 
-from .models import Profile
+from .models import Profile, User
 
+
+class UserSeralizer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        #will add a user link and profile pic to these later. 
+        feilds = ('username')
+
+#after alot of reserch I think we may want to call the followers seralizers in the views 
+#and not in the seralizer based on our current structure. 
+class ProfileSeralizer(serializers.ModelSerializer):
+    username = UserSeralizer(many=False)
+    class Meta:
+        model = Profile
+        feilds = ('id', 'username', 'email','bio','location','spotify_auth','follows')
 
